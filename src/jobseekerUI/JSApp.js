a491d7dd318eea5e55355cd4712ecb1cc1945493
ArunReddy <rakasiarunkumarreddy@gmail.com>
2025-01-13 01:04:36 +0530

    carousals added

--- src/jobseekerUI/JSApp.js
+++ src/jobseekerUI/JSApp.js
@@ -1,18 +1,11 @@
 // src/App.js (JSApp.js)
-import React, { useState } from "react";
 import Header from "./header/Header";
 import Body from "./body/body";
 import "./JSApp.css";
 import FooterComp from "../HiringManager/dashborad/footer"
+import DarkVariantExample from "../HiringManager/dashborad/carousal"
 
 const JSApp = () => {
-  const [searchTerm, setSearchTerm] = useState(""); // State to hold search term
-
-  // Function to update the search term
-  const handleSearch = (term) => {
-    setSearchTerm(term);
-  };
-
   return (
     <div
       className="app-container"
@@ -20,9 +13,9 @@ const JSApp = () => {
         background: "linear-gradient(90deg, rgb(2, 73, 108), rgb(9, 33, 69));",
       }}
     >
-      <Header onSearch={handleSearch} />{" "}
-      {/* Pass handleSearch function to Header */}
-      <Body searchTerm={searchTerm} /> {/* Pass searchTerm down to Body */}
+      <Header />
+      <Body  /> 
+      <DarkVariantExample/>
       <FooterComp/>
     </div>
   );
